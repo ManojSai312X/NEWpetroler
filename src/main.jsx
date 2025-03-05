e676be4993e641b95ba64119ad83a393bc8f9fc5
ManojSai312X <manoj23sai@gmail.com>
2025-03-05 20:37:57 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client"; // Import createRoot from react-dom/client
 import App from "./App";
 import "./index.css";
 import "./App.css";
 
-ReactDOM.render(
+// Get the root element from your HTML file
+const rootElement = document.getElementById("root");
+
+// Create a root for your app
+const root = createRoot(rootElement);
+
+// Render your app inside the root
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
